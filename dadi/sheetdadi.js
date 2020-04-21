5bf90678ab51785d5be4f0027107f12f308f9b64
Juri Di salvo <Juridisalvo11@gmail.com>
2020-04-21 20:28:39 +0200

    Mail - Dadi

---
+++ dadi/sheetdadi.js
@@ -0,0 +1,26 @@
+// chidere all'utente un numero comreso tra 0 e 6
+var user_number = parseInt(prompt('Inserire un numero compreso tra 0 e 6'));
+console.log(user_number);
+
+//generare un numero casuale compreso tra 0 e 6
+var random_number = Math.floor(Math.random() * 6) + 1;
+console.log(random_number);
+
+//condizioni
+if(isNaN(user_number) || user_number <= 0 || user_number > 6) {
+    var achievement = 'ERRORE!'
+    console.log('Numero errato!');
+} else if (user_number > random_number) {
+    var achievement = 'HAI VINTO!'
+    console.log('HAI VINTO!');
+} else if (user_number < random_number) {
+    var achievement = 'HAI PERSO!'
+    console.log('HAI PERSO!');
+} else if (user_number = random_number) {
+    var achievement = 'PAREGGIO!'
+    console.log('PAREGGIO!');
+}
+
+document.getElementById('chosen-number').innerHTML = user_number;
+document.getElementById('random-number').innerHTML = random_number;
+document.getElementById('result').innerHTML = achievement;
--- mail/sheet.js
+++ mail/sheet.js
@@ -1,15 +1,29 @@
 // chiedere email utente
-var email_utente = prompt('Inserire email utente')
-console.log(email_utente);
+var user_email = prompt('Inserire email utente')
+//console.log(user_email);
 
-//creare array con lista email
-var email_consentite = ['andrea.rossi@gmail.com', 'marco.verdi@gmail.com', 'claudio75@hotmail.it']
-console.log(email_consentite);
+//creare array con database email
+var email_list = ['andrea.rossi@gmail.com', 'marco.verdi@gmail.com', 'claudio75@hotmail.it', 'carlobianchi@gmail.com', 'max84@live.it']
+//console.log(email_list);
 
-//genera messaggio appropriato
+//impostare variabile e utilizzare il for per verificare se l'email è nel database
+var found_email = false;
 
-if (email_utente == email_consentite[0] || email_utente == email_consentite[1] || email_utente == email_consentite[2]) {
-    console.log('sei in lista');
-} else {
-    console.log('non sei in lista');
+for (var i = 0; i < email_list.length; i++) {
+
+    if (user_email == email_list[i]) {
+        found_email = true;
+    }
 }
+
+//finita la verifica generare messaggio appropriato
+if(found_email == true) {
+    var result = 'Accesso Consentito';
+    //console.log('sei in lista');
+} else {
+    var result = 'Accesso Negato';
+    //console.log('non sei in lista');
+}
+
+document.getElementById('email').innerHTML = user_email;
+document.getElementById('access').innerHTML = result;
